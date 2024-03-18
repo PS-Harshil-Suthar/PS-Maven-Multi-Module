4cd4e810250150a613643819b9db7c79d2a49b9d
PS-Harshil-Suthar <Harshil.suthar@productsquads.co>
2024-03-18 18:44:55 +0530

    Add Shared Library

---
+++ SharedLibrary/src/main/java/SharedLibrary/SharedLibrary/App.java
@@ -0,0 +1,13 @@
+package SharedLibrary.SharedLibrary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
